a6cb2c9e4dc9d984c881a033378daff7cf8e663c
swarna-dev-tech <swarnaj@gmail.com>
2026-06-02 12:22:56 +0100

    Update header text in App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className='header field'>
-      Swarna's new VAT CALCULATOR - why are you not running
+      Swarna's new VAT CALCULATOR - we will make you work
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
         <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
